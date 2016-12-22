aff58bf80a3e68861849227f1af67ac16a8f4769
Peter Hansson <peter__hansson@hotmail.com>
2016-12-22 01:45:46 +0100

    Finished assignment A

---
+++ lab1/src/a/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.File;
+
+public class Main{
+	public static void main(String[] args){
+		int element = Integer.parseInt(args[0]);
+		File file = new File(args[1]);
+		try{
+			Scanner input = new Scanner(file);
+			ArrayList<Integer> list = new ArrayList<Integer>();
+
+			while(input.hasNext()){
+				list.add(input.nextInt());	
+			}
+
+			MySortedIntArray array = new MySortedIntArray(list);
+			System.out.println(array.member(element));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ lab1/src/a/MySortedIntArray.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class MySortedIntArray implements MyIntSet{
+
+	private ArrayList<Integer> list;
+
+	public MySortedIntArray(ArrayList<Integer> list){
+		this.list = list;
+	}
+
+	public boolean member(int element){
+		int low = 0;
+		int high = list.size() - 1;
+
+		while(low <= high){
+			int mid = low + (high - low)/2;
+			if(element > this.list.get(mid)){
+				low = mid + 1;
+			}
+			else if(element < this.list.get(mid)){
+				high = mid - 1;
+			}
+			else{
+				return true;
+			}
+
+		}
+		return false;
+	}
+	
+}
