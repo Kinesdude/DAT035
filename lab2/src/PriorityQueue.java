e7d5eb469cef5008438dfff982e75b9186a07f69
Peter Hansson <peter__hansson@hotmail.com>
2016-12-24 01:15:57 +0100

    updated

--- lab2/src/PriorityQueue.java
+++ lab2/src/PriorityQueue.java
@@ -1,14 +1,15 @@
-public class PriorityQueue<E>{
+import java.util.*;
+
+public class PriorityQueue<E extends Comparator<? super E>>{
 
 	private ArrayList<E> list;
+	private Comparator<E> comp;
+	private static final int DEFAULT_CAPACITY = 10;
+	
 
-	public PriorityQueue(){
+	public PriorityQueue(Comparator comp){
+		this.comp = comp;
+		this.list = new ArrayList<E>(DEFAULT_CAPACITY);  
 
 	}
-
-	public PriorityQueue(){
-
-	}
-
-
 }
