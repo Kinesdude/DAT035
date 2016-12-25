db236d7ee88749019f4cc5261439599ac9db8cff
Peter Hansson <peter__hansson@hotmail.com>
2016-12-25 19:19:28 +0100

    Finished Bid

--- lab2/src/Bid.java
+++ lab2/src/Bid.java
@@ -1,16 +1,36 @@
 public class Bid{
 	private String name;
 	private int price;
-	private int oldPrice;
+	private Integer oldPrice;
 
-	public Bid(String name, int price){
-		this(name, -1, price);
+	public Bid(Bid bid){
+		this(bid.getName(),bid.getPrice());				
 	}
 
-	public Bid(String name, int oldPrice, int price){
+	public Bid(String name, int price){
+		this(name,null,price);
+	}
+
+	public Bid(String name, Integer oldPrice, int price){
 		this.name = name;
 		this.price = price;
 		this.oldPrice = oldPrice;
+	}
+
+	public boolean equals(Object o){
+		if(o instanceof Bid){
+			Bid bid = (Bid)o;
+			return (bid.getName().equals(this.name)  &&  
+					bid.getPrice() == this.price);
+		}	
+		return false;
+	}
+
+	public int hashCode(){
+		int hashCode = 1;
+		hashCode = hashCode * 37 + this.name.hashCode();
+		hashCode = hashCode * 41 + this.price;
+		return hashCode;
 	}
 
 	public int getPrice(){
@@ -20,5 +40,8 @@ public class Bid{
 	public String getName(){
 		return name;
 	}
-
+	
+	public boolean isNew(){
+		return this.oldPrice == null;
+	}
 }
