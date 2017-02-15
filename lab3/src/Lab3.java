c0feb44ad6d57cd5f8fd7086c65a13314e89ae4b
Peter Hansson <peter__hansson@hotmail.com>
2017-02-15 11:26:17 +0100

    Updated

--- lab3/src/Lab3.java
+++ lab3/src/Lab3.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import Lab3Help.*;
 
 public class Lab3{
 	public static void main(String[] args){
@@ -10,9 +11,9 @@ public class Lab3{
 
 	private List<Edge> edgeAdapter(List<BLineTable> lines){
 		Iterator<BLineTable> it = lines.iterator();
-		List<Edge> edges = new List();
+		List<Edge> edges = new LinkedList();
 		while(it.hasNext()){
-			BLineStop[] stops = it.next();	
+			BLineStop[] stops = it.next().getStops();	
 			for(int i = 1 ; i < stops.length ; i++){
 				edges.add(new Edge(stops[i-1].getName(),
 								   stops[i].getName(),
