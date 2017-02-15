290b4b592e7a0c885319670b6ef3003462fdc405
Peter Hansson <peter__hansson@hotmail.com>
2017-02-15 11:29:09 +0100

    Updated Lab3.java

--- lab3/src/Lab3.java
+++ lab3/src/Lab3.java
@@ -4,8 +4,14 @@ import Lab3Help.*;
 public class Lab3{
 	public static void main(String[] args){
 		Lab3File f = new Lab3File();
-		//List<BStop> stops = f.readStops(args[0]);
-		List<BLineTable> lines = f.readLines(args[0]);
+		try{
+			//List<BStop> stops = f.readStops(args[0]);
+			List<BLineTable> lines = f.readLines(args[0]);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		Grap<String> graph = new Graph(edgeAdapter(lines));
 		
 	}
 
