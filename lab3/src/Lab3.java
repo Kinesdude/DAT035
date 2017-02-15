b5cd1250e321d4128a52355e3915f26300c2d61d
Peter Hansson <peter__hansson@hotmail.com>
2017-02-15 11:20:05 +0100

    Implemented Lab3.java

--- lab3/src/Lab3.java
+++ lab3/src/Lab3.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Lab3{
 	public static void main(String[] args){
+		Lab3File f = new Lab3File();
+		//List<BStop> stops = f.readStops(args[0]);
+		List<BLineTable> lines = f.readLines(args[0]);
+		
+	}
+
+	private List<Edge> edgeAdapter(List<BLineTable> lines){
+		Iterator<BLineTable> it = lines.iterator();
+		List<Edge> edges = new List();
+		while(it.hasNext()){
+			BLineStop[] stops = it.next();	
+			for(int i = 1 ; i < stops.length ; i++){
+				edges.add(new Edge(stops[i-1].getName(),
+								   stops[i].getName(),
+								   stops[i].getTime()));
+			}
+		}
+		return edges;
 
 	}
 }
